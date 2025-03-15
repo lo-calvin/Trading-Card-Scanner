5436ca5abb5e3c697dfda45176ef87a28507aeb5
lo-calvin <lo.cal@northeastern.edu>
2025-03-15 16:46:03 -0700

    Implemented caching for card api calls

--- src/test_serial.py
+++ src/test_serial.py
@@ -1,5 +1,6 @@
 from detector import Detector
 from retriever import Retriever
+from fifocache import FIFOCache
 import re
 import matplotlib.pyplot as plt
 from PIL import Image
@@ -46,7 +47,7 @@ def get_segmented_card(mask, bbox, img):
 
     return cropped_cutout
 
-def process_card(mask, bbox, img, track_id, ret, results):
+def process_card(mask, bbox, img, track_id, ret, results, cache):
     x_min, y_min, x_max, y_max = get_bbox_corner(bbox, img)
 
     # Convert to PIL image
@@ -55,7 +56,11 @@ def process_card(mask, bbox, img, track_id, ret, results):
     # Get card id
     matches = ret.get_card_id(cropped_cutout)
     card_id = matches[0]
-    card = Card.find(card_id)
+    if card_id in cache:
+        card = cache.get(card_id)  # if in cache
+    else:
+        card = Card.find(card_id)  # if not in cache
+        cache.put(card_id, card)
 
     # Store the result
     results.append(f"Track id {track_id}: {card.name}")
@@ -65,13 +70,13 @@ def process_card(mask, bbox, img, track_id, ret, results):
     label = f"ID: {track_id} - {card.name}"
     cv2.putText(img, label, (x_min, y_min + 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
 
-def process_all_cards(masks, bboxs, img, track_ids, ret):
+def process_all_cards(masks, bboxs, img, track_ids, ret, cache):
     tasks = []
     results = []
 
     # Loop over masks and create asynchronous tasks for each card
     for i in range(len(masks)):
-        task = process_card(masks[i], bboxs[i], img, track_ids[i], ret, results)
+        task = process_card(masks[i], bboxs[i], img, track_ids[i], ret, results, cache)
         tasks.append(task)
 
     # Wait for all tasks to complete
@@ -85,6 +90,7 @@ def main():
     
     det = Detector("res\\detection_weights\\yolo11n_seg_best_10epochs.pt")
     ret = Retriever("res\\classfication_embeddings\\ResNet18_embeddings.pt")
+    cards_cache = FIFOCache(30)
 
     img = cv2.imread("res\\test.jpg")
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -96,7 +102,7 @@ def main():
     bboxs = result.boxes.xywh.data.numpy()
     track_ids = results[0].boxes.id.int().cpu().tolist()
 
-    process_all_cards(masks, bboxs, img, track_ids, ret)
+    process_all_cards(masks, bboxs, img, track_ids, ret, cards_cache)
 
     # Display the final image with bounding boxes and labels
     plt.imshow(img)
