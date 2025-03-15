07bc2a0469528f910d909562cdda4e923243329e
jordan.lewis <jordan.git.lewis@gmail.com>
2025-03-15 12:24:59 -0700

    added todos

--- src/test.py
+++ src/test.py
@@ -111,3 +111,6 @@ async def main():
 
 # Run the asynchronous main function
 asyncio.run(main())
+
+# TODO use the Card object to pull all information then extract that into a database
+# TODO make function calls to important information that nina can call into the frontend
