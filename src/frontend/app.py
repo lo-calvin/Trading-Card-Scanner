6d233299465a4d89719934d8d15f297fe14160d0
nina <lui.n@northeastern.edu>
2025-03-15 23:09:55 -0700

    added using database methods in frontend

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,15 +1,16 @@
 import streamlit as st
 import pandas as pd
 
+import sys
+import os 
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Database methods
+from backend.db_methods import get_card, populate_tables, retrieve_card_pricing_table, retrieve_pokemon_information_table
+
 st.title('Pokemon Trading Card Scanner')
 scan_tab, collection_tab = st.tabs(['Scan', 'Collection'])
-
-# Collection data
-# TODO: connect to database, example data for now
-collection_data = {
-    'Name': ['Pikachu', 'Charmander'],
-    'Price': ['$1', '$2']
-}
 
 # Initial scan info
 if 'scanned_image' not in st.session_state:
@@ -18,16 +19,19 @@ if 'scanned_data' not in st.session_state:
     st.session_state.scanned_data = None
 if 'scanned_name' not in st.session_state:
     st.session_state.scanned_name = None
+if 'scanned_id' not in st.session_state:
+    st.session_state.scanned_id = None
 
 # Send card to scan
 def scan_card(image):
     # TODO: send to model
-    # example data
-    st.session_state.scanned_data = {
-        'Name': 'Pikachu',
-        'Price': '$1'
-    }
-    st.session_state.scanned_name = st.session_state.scanned_data['Name']
+    
+    card_id = "xy1-146"
+    st.session_state.scanned_id = card_id
+    card = get_card(card_id) # call database method to get card info from id
+    st.session_state.scanned_data = card['data']
+    st.session_state.scanned_name = card['data']['name']
+
 
 # Dialog for camera
 @st.dialog('Scan from Camera')
@@ -68,13 +72,16 @@ with scan_tab:
     if st.session_state.scanned_data:
         st.subheader(f'Scanned card: {st.session_state.scanned_name}')
     col1, col2 = st.columns(2)
+    
     # Display scanned image
     with col1:
         if st.session_state.scanned_image:
             st.image(st.session_state.scanned_image)
+
     # Display scanned data
     with col2:
         if st.session_state.scanned_data:
+            # TODO: filter what data we want to display here when scanning card
             scanned_data_df = pd.DataFrame([st.session_state.scanned_data])
             scanned_data_df = scanned_data_df.transpose()
             scanned_data_df_html = scanned_data_df.to_html(header=False)
@@ -82,11 +89,36 @@ with scan_tab:
 
             # Button to add to database
             if st.button('Add to collection', type='primary'):
-                # TODO: add to database
+                # Add to database
+                populate_tables(st.session_state.scanned_id)
                 st.success('Added to collection!')
 
 # Collection tab
 with collection_tab:
     st.header('Collection')
-    st.dataframe(collection_data, hide_index=None)
+    
+    collection_prices_df = retrieve_card_pricing_table()
+    collection_info_df = retrieve_pokemon_information_table()
+
+    # Card Pricing Table
+    st.subheader('Card Pricing Table')
+    st.dataframe(
+        collection_prices_df,
+        hide_index=True,
+        use_container_width=True,
+        column_config={
+            'Image': st.column_config.ImageColumn(width='small'),
+            'Price URL': st.column_config.LinkColumn()
+        })
+    
+    # Pokemon Information Table
+    st.subheader('Pokemon Information Table')
+    st.dataframe(
+        collection_info_df,
+        hide_index=True,
+        use_container_width=True,
+        column_config={
+            'Image': st.column_config.ImageColumn(width='small'),
+            'Price URL': st.column_config.LinkColumn()
+        })
 
